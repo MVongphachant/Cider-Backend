c3e83518a8d7942739b33cb724e1904624e6eb46
Mario <m.vongphachant@live.com>
2021-04-10 17:12:12 -0400

    minor backend changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 require('./database/mongoose')
 const ciderRouter = require('./routers/cider-routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use(ciderRouter)
 
--- src/routers/cider-routes.js
+++ src/routers/cider-routes.js
@@ -2,14 +2,14 @@ const express = require('express')
 const Cider = require('../models/cider')
 const router = new express.Router()
 
-router.post('/ciders', async (req, res) => {
+router.post('/api/ciders', async (req, res) => {
     const cider = new Cider(req.body)
 
     try {
         await cider.save()
-        res.status(201).send(cider)
+        res.status(201).json(cider)
     } catch (error) {
-        res.send(400).send(error)
+        res.send(400).json(error)
     }
 })
 
